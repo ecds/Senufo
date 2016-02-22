6bc060cf4c3fc5f1c857f107243903ad73fb94ae
Joanna <joanna@ecds-staffmac-01.library.emory.ddns>
2016-02-22 17:05:03 -0500

    notes

--- Senufo_App/views.py
+++ Senufo_App/views.py
@@ -34,7 +34,7 @@ def map_working_collection(request):
     PlaceLocationCollection = Objects_Places_Reason.objects.values('Reason_id', 'ReasonForPlace', 'Objects_Name__Work_Name', 'Objects_Name__Description', 'Objects_Name__Author__Author_Name', 'Objects_Name__AuthorAttributionCertainty', 'Objects_Name__Work_Creation_date', 'Related_Image__Image_Name', 'Related_Image__ImageAuthor_Name__Author_Name', 'Related_Image__stable_url', 'Places_Name__Map_Place_Name', 'Places_Name__Latitude', 'Places_Name__Longitude').filter(ReasonForPlace='Collection')
     return render_to_csv_response(PlaceLocationCollection)
 
-# Still working on this.
+# Still working on this.  How to link in-lines - alternate place names, provenance fields. How to link to essay url? = All 3 are backwards ForeignKey. Check into a mySQL view (workbench) for this!!
 def mapped_work_page_view(request):
     WorkPageInformation = Objects_Places_Reason.objects.values('Reason_id', 'Objects_Name__Work_Name', 'Objects_Name__Description', 'Objects_Name__Author__Author_Name', 'Objects_Name__AuthorAttributionCertainty', 'Objects_Name__Work_Creation_date', 'Objects_Name__Publication_Information', 'Objects_Name__Collection_Name', 'Objects_Name__Collection_Number', 'Objects_Name__Reported_field_acquisition_name', 'Objects_Name__Reported_field_acquisition_location__Map_Place_Name', 'Objects_Name__Reported_field_acquisition_date', 'Related_Image__ImageAuthor_Name__Author_Name')
     return render_to_csv_response(WorkPageInformation)
